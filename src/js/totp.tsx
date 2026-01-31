2ab5e6afbafc4f9b8d67ee48e1f9f48c17bb974f
Andrew F <239586353+aloe-beta@users.noreply.github.com>
2026-01-30 23:29:08 -0600

    Integrated new TOTP internal structure

--- src/js/totp.tsx
+++ src/js/totp.tsx
@@ -1,4 +1,3 @@
-// This file is a horrible mess.
 import { h, Fragment } from './dom.ts';
 import { authenticate, decrypt, encrypt, totp } from './crypto.ts';
 import { Input, post, SegmentedSelect } from './global.tsx';
@@ -8,7 +7,8 @@ if (dek === undefined || username === undefined || sessionId === undefined) {
     window.location.href = '/login';
 }
 
-// WIP (currently deadcode): Building a better data structure for this mess.
+// TODO: Fix 60s interval
+
 type totpEntry = { name: string, interval: number, secret: string };
 type totpCard = {
     name: string,
@@ -17,7 +17,7 @@ type totpCard = {
     card: HTMLElement,
     codeElmt: HTMLElement,
     donutElmt: HTMLElement,
-    nameElmt: HTMLElement,
+    nameElmt: Text,
     editElmt: HTMLElement
 };
 
@@ -99,12 +99,12 @@ class Totp {
         donutElmt.setAttribute('label', (interval - (now / 1000 % interval) >>> 0).toString());
     }
 
-    addCard(entry: totpEntry, update = true) {
+    addCard(entry: totpEntry, update = true, append = true) {
         // Create card
         const codeElmt = <span class='code'>___-___</span>;
         const donutElmt = <div class='donut' label='0'></div>;
         const editElmt = <a class='edit'>Edit</a>;
-        const nameElmt = <>{entry.name}</>;
+        const nameElmt = document.createTextNode(entry.name);
 
         const cardElmt = <div class='totp card' interval={entry.interval}>
             <div class='header'>{nameElmt}{editElmt}</div>
@@ -131,8 +131,10 @@ class Totp {
         });
 
         // Append and update
-        this.container.appendChild(cardElmt);
-        this.cards.push(card);
+        if (append) {
+            this.container.appendChild(cardElmt);
+            this.cards.push(card);
+        }
 
         if (update) {
             this.updateCode(card);
@@ -142,14 +144,70 @@ class Totp {
     }
     editCard(card: totpCard | null) {
         const menu = this.menu;
+        const { name, secret, interval, save: saveBtn, delete: deleteBtn, menu: menuElmt } = menu;
         if (card) {
-            menu.name.value = card.name;
-            menu.secret.value = card.secret;
-            menu.interval[card.interval === 30 ? 0 : 1].checked = true;
-            menu.save.classList.toggle('disabled', false);
+            name.value = card.name;
+            secret.value = card.secret;
+            interval[card.interval === 30 ? 0 : 1].checked = true;
+            saveBtn.classList.toggle('disabled', false);
+        } else {
+            name.value = '';
+            secret.value = '';
         }
 
-        menu.menu.style.display = 'block';
+        name.addEventListener('input', validate);
+
+        let valid = false;
+        function validate() {
+            valid = name.value !== '' && secret.value !== '';
+            saveBtn.classList.toggle('disabled', !valid);
+        }
+
+        menuElmt.style.display = 'block';
+        saveBtn.onclick = () => {
+            validate();
+            if (!valid) return;
+
+            const intervalValue = interval[0].checked ? 30 : 60;
+
+            if (card) {
+                card.name = name.value;
+                card.secret = secret.value;
+                card.interval = intervalValue;
+
+                card.nameElmt.textContent = name.value;
+                this.updateCode(card);
+                this.updateCounter(card);
+            } else {
+                this.addCard({ name: name.value, secret: secret.value, interval: intervalValue });
+            }
+
+            const cards = this.cards;
+
+            this.save();
+            menuElmt.style.display = 'none';
+        }
+
+        deleteBtn.onclick = () => {
+            if (card) {
+                this.cards.splice(this.cards.indexOf(card), 1);
+                card.card.remove();
+                this.save();
+            }
+            menuElmt.style.display = 'none';
+        }
+    }
+
+    save() {
+        const cards = this.cards;
+
+        setData(JSON.stringify(this.cards.map(card => {
+            return {
+                name: card.name,
+                secret: card.secret,
+                interval: card.interval
+            }
+        })));
     }
 
     static editMenu() {
@@ -180,103 +238,6 @@ class Totp {
     }
 }
 
-// function TotpCard(options: Record<'interval' | 'secret' | 'name' | 'index', any>) {
-//     const codeElmt = <span class='code'>___-___</span>;
-//     const donut = <div class='donut' label='0'></div>;
-
-//     async function updateCode() {
-//         const code = await totp(options.secret);
-//         codeElmt.textContent = code.substring(0, 3) + '-' + code.substring(3);
-//         donut.classList.remove('animated');
-//         void donut.offsetWidth; // This resets the animation.
-//         donut.classList.add('animated');
-//     }
-
-//     const interval = options.interval * 1000;
-//     const elapsed = Date.now() % interval;
-
-//     donut.style.animationDelay = `-${elapsed}ms`;
-//     donut.classList.add('animated');
-
-//     updateCode();
-//     setTimeout(() => {
-//         donut.style.animationDelay = `0s`;
-//         updateCode();
-//         setInterval(updateCode, interval);
-//     }, interval - elapsed);
-    
-
-//     syncedInterval(() => {
-//         donut.setAttribute('label', (interval / 1000 - (Date.now() % interval / 1000) >>> 0).toString());
-//     }, 1000);
-
-//     const editElmt = <a class='edit'>Edit</a>;
-//     editElmt.addEventListener('click', event => {
-//         editEntry(options.index);
-//     });
-
-//     return (
-//         <div class='totp card' interval={options.interval}>
-//             <div class='header'>{options.name}{editElmt}</div>
-//             {codeElmt}
-//             {donut}
-//         </div>
-//     );
-// }
-
-// function TotpMenu() {
-//     const nameElmt = <Input id='name' placeholder='Service name' autocomplete='off' />;
-//     const secretElmt = <Input id='secret' placeholder='Secret' autocomplete='off' />
-//     const intervalElmt = <Input id='interval' placeholder='Interval' value='30' type='number' autocomplete='off' />;
-//     const saveBtn = <button class='btn disabled'>Save</button>;
-//     const deleteBtn = <button class='btn tertiary'>Delete</button>;
-
-//     const name = nameElmt.children[0] as HTMLInputElement;
-//     const secret = secretElmt.children[0] as HTMLInputElement;
-//     const interval = intervalElmt.children[0] as HTMLInputElement;
-
-//     for (const elmt of [name, secret, interval]) {
-//         elmt.addEventListener('input', validate);
-//     }
-
-//     function validate() {
-//         saveBtn.classList.toggle('disabled', !(name.value && secret.value && interval.value));
-//     }
-
-//     saveBtn.addEventListener('click', event => {
-//         if (saveBtn.classList.contains('disabled')) {
-//             for (const elmt of [name, secret, interval]) {
-//                 if (!elmt.value) {
-//                     elmt.focus()
-//                     return;
-//                 }
-//             }
-//             // Don't return if it's somehow broken (it is)
-//             // return;
-//         }
-
-//         saveEntry()
-//     });
-
-//     deleteBtn.addEventListener('click', event => {
-//         entries.splice(entry, 1);
-//         setData(JSON.stringify(entries));
-//         window.location.reload();
-//     });
-
-//     return (
-//         <div class='card l overlay center-anchor consume-focus'>
-//             <h2>Authenticator</h2>
-//             <p>Enter the details for the application.</p>
-//             {nameElmt}
-//             {secretElmt}
-//             {intervalElmt}
-//             {saveBtn}
-//             {deleteBtn}
-//         </div>
-//     );
-// }
-
 async function getData() {
     const contents = (await post('/api/db', { sessionId: sessionId.toBase64(), key: 'totp' })).contents;
     if (contents) {
@@ -290,71 +251,24 @@ async function setData(data: string) {
     return post('/api/db', { sessionId: sessionId.toBase64(), key: 'totp', value: encrypted });
 }
 
-let data = await getData();
-let entries = data ? JSON.parse(data) : [];
+async function App() {
+    let data = await getData();
+    let entries = data ? JSON.parse(data) : [];
 
-const app = new Totp(entries);
-document.body.appendChild(app.container);
+    const app = new Totp(entries);
 
-// let entry = 0;
-// const menu = <TotpMenu />;
-// menu.style.display = 'none';
+    const newButton = <button class='btn square'>+</button>;
+    newButton.addEventListener('click', event => {
+        app.editCard(null);
+    });
 
-// function editEntry(index: number) {
-//     menu.style.display = 'block';
-//     entry = index;
-
-//     const service = entries[index];
-//     (document.getElementById('name') as HTMLInputElement).value = service?.name || '';
-//     (document.getElementById('secret') as HTMLInputElement).value = service?.secret || '';
-//     (document.getElementById('interval') as HTMLInputElement).value = service?.interval.toString() || '30';
-// }
-
-// function saveEntry() {
-//     menu.style.display = 'none';
-//     const name = (document.getElementById('name') as HTMLInputElement);
-//     const secret = (document.getElementById('secret') as HTMLInputElement);
-//     const interval = (document.getElementById('interval') as HTMLInputElement);
-
-//     entries[entry] = { name: name.value, interval: parseFloat(interval.value), secret: secret.value }
-//     setData(JSON.stringify(entries));
-
-//     // TODO: Add dynamically
-//     window.location.reload();
-// }
-
-// function syncedInterval(callback: () => void, interval: number, delayExec = false) {
-//     if (!delayExec) callback();
-
-//     const elapsed = Date.now() % interval;
-
-//     setTimeout(() => {
-//         callback();
-//         setInterval(callback, interval);
-//     }, interval - elapsed);
-// }
-
-// async function App() {
-//     const container = <div class='margin-center' id='container' style='width: min-content'></div>;
-//     let i = 0;
-//     for (const service of entries) {
-//         container.append(<TotpCard name={service.name} interval={service.interval} secret={service.secret} index={i++} />);
-//     }
-
-//     const newButton = <button class='btn square'>+</button>;
-//     newButton.addEventListener('click', event => {
-//         editEntry(entries.length);
-//     });
-
-//     return (<>
-//         <div class='topnav'></div>
-//         {container}
-//         <div class='sp' style='margin-top: 88px'></div>
-//         <div class='se-anchor'>
-//             {newButton}
-//         </div>
-//         {menu}
-//     </>);
-// }
-
-// document.body.append(await App());
+    return (<>
+        <div class='topnav'></div>
+        {app.container}
+        <div class='sp' style='margin-top: 88px'></div>
+        <div class='se-anchor'>
+            {newButton}
+        </div>
+    </>);
+}
+document.body.appendChild(await App());
